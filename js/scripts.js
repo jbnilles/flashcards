cb175ed81ceb6c8e5c7ceffc71279becc4ecfe7f
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-11 13:38:11 -0700

    made changes so that the clickable fuction would work.
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // $(".clickable").click(function(){
+  //   $("#showing").toggle();
+  //   $("#hidden").toggle();
+  // });
+  $(".card").click(function(){
+    $("p",this).toggle();
+    
+  });
+
+
+
+
+});
